ba05eada1d6a8bf5d92c11b9c6d2eee11a8e0bb6
alexzhf <alex.hf.zhang@gmail.com>
2019-09-15 13:30:05 +0800

    Update ch5's code

---
+++ ch5/3.py
@@ -0,0 +1,8 @@
+print('This program converts Fahrenheit to Celsius.')
+print('Type in Fahrenheit:', end=' ')
+fahrenheit = float(input())
+celsius = (fahrenheit - 32) * 5.0 / 9
+print('That is', end=' ')
+print(celsius)
+print('degrees Celsius')
+#输入小明年龄，他爸爸和爷爷分别大30.5
