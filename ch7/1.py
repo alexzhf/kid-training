c8afe47d6befe208e8f8f31d1e5dce28d61f36f4
Alex Zhang <alex.hf.zhang@gmail.com>
2019-11-17 15:03:01 +0800

    Update Ch7

---
+++ ch7/1.py
@@ -0,0 +1,13 @@
+dan_age=12
+eric_age=13
+
+if dan_age==eric_age:
+    print("Dan and Eric are at the same age!")
+elif dan_age>eric_age:
+    print("Dan is older than Eric")
+    print("Their age gap is： "+str(dan_age-eric_age))
+else:
+    print("Eric is older than Dan")
+    print("Their age gap is： "+str(eric_age-dan_age))
+
+print("The is the end of program!")
---
+++ ch7/2.py
@@ -0,0 +1,6 @@
+age = float(input("Enter your age: "))
+grade = float(input("Enter your grade: "))
+if age >=8 and grade >=3:
+    print("You can play this game.")
+else:
+    print("You are too young. Can't play this game.")
